0158f76574c8d2fe25ddf473afa9520107f562bc
Victor Sun <pvz2loveyou@gmail.com>
2020-01-08 23:01:35 +0800

    Surface First Commit

--- test.py
+++ test.py
@@ -2,7 +2,8 @@
 @Author: Victor Sun
 @Github: https://github.com/victorsun0926/
 @Date: 2020-01-08 22:22:13
-@LastEditTime : 2020-01-08 22:42:42
+@LastEditTime : 2020-01-08 23:01:11
 '''
 x = 10
+print(x)
 print("你好，世界")
