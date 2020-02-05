bf2fefe07069c2a4b91c76e54238bb9753e31467
Victor Sun <pvz2loveyou@gmail.com>
2020-02-05 12:17:43 +0800

    Test01

--- Java/20200205/Test/JustTest.java
+++ Java/20200205/Test/JustTest.java
@@ -1,6 +1,28 @@
-public class JustTest {
+/*
+ * @Author: Victor Sun
+ * @Github: https://github.com/victorsun0926/
+ * @Date: 2020-02-05 10:01:02
+ * @LastEditTime : 2020-02-05 12:15:59
+ */
 
-	public static void main(String[] args) {
-		System.out.print("Hello World.");
+import java.io.IOException;
+import java.util.Scanner;
+ public class JustTest {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Hello World.");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入成绩：");
+		int score = sc.nextInt();
+		if(score < 0 | score > 300) {
+        System.out.println("成绩无效");
+		}
+		else if(score >= 0 & score < 210) {
+			System.out.println("考试不通过");
+		}
+		else if(score > 210 & score <=300) {
+			System.out.println("考试通过");
+		}
+		sc.close();
 	}
 }
