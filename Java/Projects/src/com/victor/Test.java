ab69291469ff45b9b9f6ac1df5c752d413d27521
Victor Sun <pvz2loveyou@gmail.com>
2021-04-20 19:44:01 +0800

    java_homework

--- Java/Projects/src/com/victor/Test.java
+++ Java/Projects/src/com/victor/Test.java
@@ -1,25 +1,86 @@
 package com.victor;
 
-import java.util.Scanner;
-
-public class Main {
+public class Test {
 
     public static void main(String[] args) {
-        FOne f1 = new FOne();
-        System.out.print("Please Input a number: ");
-        int x;
-        Scanner s = new Scanner(System.in);
-        x = s.nextInt();
-        f1.run(x);
-        FTwo f2 = new FTwo();
-        f2.run();
-        FThree f3 = new FThree();
-        f3.run();
-        FFour f4 = new FFour();
-        f4.run();
+        fOne f = new fOne();
+        f.output();
+
+        CPU cpu = new CPU();
+        cpu.setSpeed(2200);
+
+        HardDisk disk = new HardDisk();
+        disk.setAmount(200);
+
+        PC pc = new PC();
+        pc.setCPU(cpu);
+        pc.setHardDisk(disk);
+        pc.show();
     }
 }
 
 class fOne{
-    
+    private char alpha;
+    fOne(){
+        alpha = 'A';
+    }
+    void output(){
+        for (int i = 0; i < 26; i++) {
+            System.out.print(alpha+" ");
+            alpha++;
+        }
+        System.out.println();
+    }
+}
+
+class CPU{
+    private int speed;
+
+    CPU(){
+        speed = 0;
+    }
+
+    public void setSpeed(int m){
+        this.speed = m;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+class HardDisk{
+    private int amount;
+
+    HardDisk(){
+        amount = 0;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int m) {
+        this.amount = m;
+    }
+
+}
+
+class PC{
+    private CPU c;
+    private HardDisk HD;
+
+    public void setCPU(CPU c) {
+        this.c = c;
+    }
+
+    public void setHardDisk(HardDisk d) {
+        this.HD = d;
+    }
+
+    void show(){
+        System.out.println("CPU speed: "+ c.getSpeed());
+        System.out.println("HardDisk amount: "+ HD.getAmount());
+    }
+
 }
